55378fbf6a9b4dbe7bba5a926b268987b73a33bd
Sindhupriya Kamaraj <Sindhupriyakamaraj@gmail.com>
2025-10-08 20:36:00 +0530

    first commit

---
+++ helloworld.py
@@ -0,0 +1,24 @@
+'''a ="hello"
+b= "world"
+c= a+b
+print(c)'''
+ 
+T= int(input("Tamil: "))
+E = int(input("English: "))
+M = int(input("Maths: "))
+SC = int(input("Science: "))
+S = int(input("Social: "))
+T= T+E+M+SC+S
+AVG = T/5
+print (T)
+if T<250:
+    print("GRADE C")
+elif(T<400):
+    print("GRADE B")
+else:
+    print("GRADE A")
+
+
+
+
+
