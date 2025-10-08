f22ce11991ea33c918eeb4a0336e32ee6e12514d
Sindhupriya Kamaraj <Sindhupriyakamaraj@gmail.com>
2025-10-08 20:40:09 +0530

    small changes

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,9 @@
 b= "world"
 c= a+b
 print(c)'''
- 
+
+N=input("Name:")
+C=input("Class: ") 
 T= int(input("Tamil: "))
 E = int(input("English: "))
 M = int(input("Maths: "))
@@ -10,13 +12,15 @@ SC = int(input("Science: "))
 S = int(input("Social: "))
 T= T+E+M+SC+S
 AVG = T/5
-print (T)
+print ("Total:",T)
+print ("Average:",AVG)
 if T<250:
     print("GRADE C")
 elif(T<400):
     print("GRADE B")
 else:
     print("GRADE A")
+
 
 
 
